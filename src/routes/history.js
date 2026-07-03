f55c4fe77c4423b0677bdf0e67a6d636934968df
Aamir Khan Pathan <programmingpathanaa@gmail.com>
2026-07-03 09:14:09 -0400

    fixed history

--- src/routes/history.js
+++ src/routes/history.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const DB_PATH = "./src/db/activity.json";
 
 router.get("/", (req, res) => {
-    const page = Nmber(req.query.page) || 1;
+    const page = Number(req.query.page) || 1;
     const pageSize = 5;
 
     const data = JSON.parse(fs.readFileSync(DB_PATH, "utf8"));
